729fc4cb16d1d4e16366376e20f2a06ee86376ba
Michael Farias de Lima (UFC) <michaelfarias@insightlab.ufc.br>
2024-10-23 16:52:46 -0300

    removing unused migration

--- todo/db/migrate/20241022022604_rename_show_card_columns.rb
+++
@@ -1,7 +0,0 @@
-class RenameShowCardColumns < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :users, :show_priority, :show_card_priority
-    rename_column :users, :show_due_date, :show_card_due_date
-    rename_column :users, :show_mood, :show_card_mood
-  end
-end
