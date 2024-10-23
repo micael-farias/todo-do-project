7ad1eceb553be6d258bd3d33b1ec920cd4202ff3
Michael Farias de Lima (UFC) <michaelfarias@insightlab.ufc.br>
2024-10-23 16:24:17 -0300

    fixing class board duplication

--- todo/app/services/boards/board_duplication_service.rb
+++ todo/app/services/boards/board_duplication_service.rb
@@ -1,26 +1,25 @@
 module Boards
   class BoardDuplicationService
-      def initialize(board)
-        @original_board = board
-      end
-    
-      def duplicate
-        new_board = @original_board.dup
-        new_board.title = "#{@original_board.title} - Cópia"
-    
-        ActiveRecord::Base.transaction do
-          if new_board.save
-            @original_board.board_items.each do |item|
-              new_board.board_items.create!(name: item.name, position: item.position)
-            end
-            new_board
-          else
-            raise ActiveRecord::Rollback, "Erro ao duplicar o board."
-          end
-        end
-      rescue ActiveRecord::Rollback
-        nil
-      end
+    def initialize(board)
+      @original_board = board
     end
-  end   
-  
+
+    def duplicate
+      new_board = @original_board.dup
+      new_board.title = "#{@original_board.title} - Cópia"
+      
+      ActiveRecord::Base.transaction do
+        if new_board.save
+          @original_board.board_items.each do |item|
+            new_board.board_items.create!(name: item.name, position: item.position)
+          end
+          new_board
+        else
+          raise ActiveRecord::Rollback, "Erro ao duplicar o board."
+        end
+      end
+    rescue ActiveRecord::Rollback
+      nil
+    end
+  end
+end
