4e9558245dd5cf4eb2c6541fc8b2a28fc0eac41c
Michael Farias de Lima (UFC) <michaelfarias@insightlab.ufc.br>
2024-10-28 08:06:24 -0300

    fixing js

--- todo/app/javascript/boards/show.js
+++ todo/app/javascript/boards/show.js
@@ -1,3 +1,5 @@
+import { consolidateColumnEdit } from "../board_items/save";
+
 $(document).ready(function () {
 
   $('#duplicate-board-button').on('click', function (e) {
@@ -176,4 +178,70 @@ $(document).ready(function () {
     });
     inputGroup.replaceWith(updatedSpan);
   }
+
+  $('#kanban-board').on('click', '.column-name', function () {
+    var span = $(this);
+    var currentName = span.text();
+
+    var input = $('<input>', {
+      type: 'text',
+      class: 'form-control column-name-input',
+      value: currentName
+    });
+
+    span.replaceWith(input);
+    input.focus();
+
+    input.select();
+  });
+
+  $('#kanban-board').on('keydown', '.column-name-input', function (e) {
+    var input = $(this);
+    if (e.which == 13) {
+      e.preventDefault();
+      consolidateColumnEdit(input);
+    }
+  });
+
+  $(document).on('click', '.delete-column-button', function (e) {
+    e.stopPropagation();
+    var button = $(this);
+    var columnId = button.data('column-id');
+    var column = button.closest('.column');
+
+    var hasCards = column.find('.card').length > 0;
+    var confirmationMessage = hasCards ?
+      'Esta coluna contém cards. Ao excluir a coluna, todos os cards também serão excluídos. Deseja continuar?' :
+      'Tem certeza que deseja excluir esta coluna?';
+
+    if (confirm(confirmationMessage)) {
+      var boardId = $('#kanban-board').data('board-id');
+      var deleteUrl = '/boards/' + boardId + '/board_items/' + columnId;
+
+      $.ajax({
+        url: deleteUrl,
+        method: 'DELETE',
+        success: function (response) {
+          if (response.success) {
+            column.fadeOut(300, function () {
+              $(this).remove();
+            });
+          } else {
+            alert('Erro ao excluir a coluna: ' + response.message);
+          }
+        },
+        error: function () {
+          alert('Erro ao excluir a coluna. Por favor, tente novamente.');
+        }
+      });
+    }
+  });
+
+  $('.show-new-card-form').on('click', function (e) {
+    e.preventDefault();
+    var column = $(this).closest('.column');
+    column.find('.add-card-button').hide();
+    column.find('.new-card-form').show();
+    column.find('.card-title-input').focus();
+  });
 })
